03fbffdf3746b1dd5dc627721ab55fc413c1e50a
Brian Tu <tub4@uci.edu>
2023-11-09 20:50:47 -0800

    added template info

--- src/pages/FuturePage.tsx
+++ src/pages/FuturePage.tsx
@@ -1,8 +1,8 @@
-import { Container } from "@mui/material";
+import { Box, Card, CardMedia, Container, Typography } from "@mui/material";
 
 const FuturePage = () => {
     return (
-        <Container sx={{
+        <Container sx={{ 
             marginTop: "74px", 
             minWidth: "100%",
             marginRight: "0px",
@@ -11,8 +11,32 @@ const FuturePage = () => {
                 paddingLeft: "0px",
                 paddingRight: "0px",
             },
-        }}>
-            <p>this is future page</p>
+            }}>
+            <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between", padding: "0px", background: "black", paddingBlock: "50px" }}>
+                <Box sx={{
+                    display: "flex", 
+                    flexDirection: "column", 
+                    alignItems: "left", 
+                    justifyContent: "left", 
+                    textAlign: "left", 
+                    marginLeft: "10%",
+                    marginRight: "10%"
+                }}>
+                    <Typography sx={{ fontSize: 50, color: "white", fontStyle: "italic", alignItems: 'center'}}>Our Future Plans</Typography>
+                    <Typography sx={{ fontSize: 30, color: "white" }}>
+                    Our cutting-edge furniture customization platform offers users the unique opportunity to design and personalize their dream living spaces in an immersive virtual environment, all while collaborating with others to shape the future of interior design. With a bright horizon ahead, we're actively developing new features and innovations to redefine the way people furnish their homes, making this investment an exciting gateway to the future of personalized home design.                    </Typography>
+                </Box>
+                <Box
+                    component="img"
+                    sx={{
+                        height: 400,
+                        width: 700,
+                        marginRight: "10%",
+                    }}
+                    alt="The house from the offer."
+                    src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
+                />
+            </Box>
         </Container>
     );
 };
--- src/pages/InvestPage.tsx
+++ src/pages/InvestPage.tsx
@@ -1,8 +1,8 @@
-import { Container } from "@mui/material";
+import { Box, Card, CardMedia, Container, Typography } from "@mui/material";
 
 const InvestPage = () => {
     return (
-        <Container sx={{
+        <Container sx={{ 
             marginTop: "74px", 
             minWidth: "100%",
             marginRight: "0px",
@@ -11,8 +11,33 @@ const InvestPage = () => {
                 paddingLeft: "0px",
                 paddingRight: "0px",
             },
-        }}>
-            <p>this is invest page</p>
+            }}>
+            <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between", padding: "0px", background: "black", paddingBlock: "50px" }}>
+                <Box sx={{
+                    display: "flex", 
+                    flexDirection: "column", 
+                    alignItems: "left", 
+                    justifyContent: "left", 
+                    textAlign: "left", 
+                    marginLeft: "10%",
+                    marginRight: "10%"
+                }}>
+                    <Typography sx={{ fontSize: 50, color: "white", fontStyle: "italic", alignItems: 'center'}}>Why Invest</Typography>
+                    <Typography sx={{ fontSize: 30, color: "white" }}>
+                    Invest in our innovative furniture customization platform, where users can create personalized home furnishings in a virtual, interactive environment, and collaborate with others to shape the future of personalized living spaces. Join us on this exciting journey to revolutionize the way people design and furnish their homes.
+                    </Typography>
+                </Box>
+                <Box
+                    component="img"
+                    sx={{
+                        height: 400,
+                        width: 700,
+                        marginRight: "10%",
+                    }}
+                    alt="The house from the offer."
+                    src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
+                />
+            </Box>
         </Container>
     );
 };
